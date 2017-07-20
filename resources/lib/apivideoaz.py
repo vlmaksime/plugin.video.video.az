372dfd8805189bf616045229764fd99fa73e3dc9
Vladimir Maksimenko <vl.maksime@gmail.com>
2017-07-20 16:00:44 +0300

    v0.1.6

--- resources/lib/apivideoaz.py
+++ resources/lib/apivideoaz.py
@@ -3,6 +3,7 @@
 # License: GPL v.3 https://www.gnu.org/copyleft/gpl.html
 
 import requests
+import re
 
 class VideoAzApiError(Exception):
     def __init__(self, value, code):
@@ -486,7 +487,7 @@ class videoaz:
                        'video_quality': movie['video_quality'],
                        'audio_quality': movie['audio_quality'],
                        'duration': duration_sec,
-                       'plot':     movie['description'],
+                       'plot':     self.__remove_html(movie['description']),
                        'mpaa':     self.__get_mpaa(movie['age_restriction'])}
         else:
             details = {}
@@ -505,3 +506,101 @@ class videoaz:
             return 'NC-17'
         else:
             return ''
+
+    def __remove_html( self, text ):
+        result = text
+        result = result.replace(u'&nbsp;',      u' ')
+        result = result.replace(u'&pound;',     u'£')
+        result = result.replace(u'&euro;',      u'€')
+        result = result.replace(u'&para;',      u'¶')
+        result = result.replace(u'&sect;',      u'§')
+        result = result.replace(u'&copy;',      u'©')
+        result = result.replace(u'&reg;',       u'®')
+        result = result.replace(u'&trade;',     u'™')
+        result = result.replace(u'&deg;',       u'°')
+        result = result.replace(u'&plusmn;',    u'±')
+        result = result.replace(u'&frac14;',    u'¼')
+        result = result.replace(u'&frac12;',    u'½')
+        result = result.replace(u'&frac34;',    u'¾')
+        result = result.replace(u'&times;',     u'×')
+        result = result.replace(u'&divide;',    u'÷')
+        result = result.replace(u'&fnof;',      u'ƒ')
+        result = result.replace(u'&Alpha;',     u'Α')
+        result = result.replace(u'&Beta;',      u'Β')
+        result = result.replace(u'&Gamma;',     u'Γ')
+        result = result.replace(u'&Delta;',     u'Δ')
+        result = result.replace(u'&Epsilon;',   u'Ε')
+        result = result.replace(u'&Zeta;',      u'Ζ')
+        result = result.replace(u'&Eta;',       u'Η')
+        result = result.replace(u'&Theta;',     u'Θ')
+        result = result.replace(u'&Iota;',      u'Ι')
+        result = result.replace(u'&Kappa;',     u'Κ')
+        result = result.replace(u'&Lambda;',    u'Λ')
+        result = result.replace(u'&Mu;',        u'Μ')
+        result = result.replace(u'&Nu;',        u'Ν')
+        result = result.replace(u'&Xi;',        u'Ξ')
+        result = result.replace(u'&Omicron;',   u'Ο')
+        result = result.replace(u'&Pi;',        u'Π')
+        result = result.replace(u'&Rho;',       u'Ρ')
+        result = result.replace(u'&Sigma;',     u'Σ')
+        result = result.replace(u'&Tau;',       u'Τ')
+        result = result.replace(u'&Upsilon;',   u'Υ')
+        result = result.replace(u'&Phi;',       u'Φ')
+        result = result.replace(u'&Chi;',       u'Χ')
+        result = result.replace(u'&Psi;',       u'Ψ')
+        result = result.replace(u'&Omega;',     u'Ω')
+        result = result.replace(u'&alpha;',     u'α')
+        result = result.replace(u'&beta;',      u'β')
+        result = result.replace(u'&gamma;',     u'γ')
+        result = result.replace(u'&delta;',     u'δ')
+        result = result.replace(u'&epsilon;',   u'ε')
+        result = result.replace(u'&zeta;',      u'ζ')
+        result = result.replace(u'&eta;',       u'η')
+        result = result.replace(u'&theta;',     u'θ')
+        result = result.replace(u'&iota;',      u'ι')
+        result = result.replace(u'&kappa;',     u'κ')
+        result = result.replace(u'&lambda;',    u'λ')
+        result = result.replace(u'&mu;',        u'μ')
+        result = result.replace(u'&nu;',        u'ν')
+        result = result.replace(u'&xi;',        u'ξ')
+        result = result.replace(u'&omicron;',   u'ο')
+        result = result.replace(u'&pi;',        u'π')
+        result = result.replace(u'&rho;',       u'ρ')
+        result = result.replace(u'&sigmaf;',    u'ς')
+        result = result.replace(u'&sigma;',     u'σ')
+        result = result.replace(u'&tau;',       u'τ')
+        result = result.replace(u'&upsilon;',   u'υ')
+        result = result.replace(u'&phi;',       u'φ')
+        result = result.replace(u'&chi;',       u'χ')
+        result = result.replace(u'&psi;',       u'ψ')
+        result = result.replace(u'&omega;',     u'ω')
+        result = result.replace(u'&larr;',      u'←')
+        result = result.replace(u'&uarr;',      u'↑')
+        result = result.replace(u'&rarr;',      u'→')
+        result = result.replace(u'&darr;',      u'↓')
+        result = result.replace(u'&harr;',      u'↔')
+        result = result.replace(u'&spades;',    u'♠')
+        result = result.replace(u'&clubs;',     u'♣')
+        result = result.replace(u'&hearts;',    u'♥')
+        result = result.replace(u'&diams;',     u'♦')
+        result = result.replace(u'&quot;',      u'"')
+        result = result.replace(u'&amp;',       u'&')
+        result = result.replace(u'&lt;',        u'<')
+        result = result.replace(u'&gt;',        u'>')
+        result = result.replace(u'&hellip;',    u'…')
+        result = result.replace(u'&prime;',     u'′')
+        result = result.replace(u'&Prime;',     u'″')
+        result = result.replace(u'&ndash;',     u'–')
+        result = result.replace(u'&mdash;',     u'—')
+        result = result.replace(u'&lsquo;',     u'‘')
+        result = result.replace(u'&rsquo;',     u'’')
+        result = result.replace(u'&sbquo;',     u'‚')
+        result = result.replace(u'&ldquo;',     u'“')
+        result = result.replace(u'&rdquo;',     u'”')
+        result = result.replace(u'&bdquo;',     u'„')
+        result = result.replace(u'&laquo;',     u'«')
+        result = result.replace(u'&raquo;',     u'»')
+
+        # result = result.replace(u'<br>',    u'\n')
+
+        return re.sub('<[^<]+?>', '', result)
